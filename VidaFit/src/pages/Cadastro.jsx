abcfaf574695a4ba3dc512c1aa81352f0d13f573
GUILHERME ZAMBONI MENEGACIO <guilherme_menegacio@estudante.sesisenai.org.br>
2025-03-13 14:25:19 -0300

    Tela Cadastro

--- VidaFit/src/pages/Cadastro.jsx
+++ VidaFit/src/pages/Cadastro.jsx
@@ -6,13 +6,26 @@ function Cadastro() {
   return (
     
     <div className='container-cadastro'>
-    <Navbar />
-      <div className='div-cadas'>
+      <Navbar />
+        <div className='div-cadas'>
+        <div className="espaco"></div>
+        
+        <div className='cadas-inf'>
+          <div className="cadas-input">
+              <input className='texto-cadas' type="text" placeholder='Nome :'/>
+              <input className='texto-cadas' type="text" placeholder='Data de nascimento :'/>
+              <input className='texto-cadas' type="text" placeholder='Email :'/>
+              <input className='texto-cadas' type="text" placeholder='Senha :'/>
+              <input className='texto-cadas' type="text" placeholder='Comfirmar Senha :'/>
+              <div className="espaco"></div>
+            </div>
 
-
+              <button className='button'>cadastrar</button>
+          
+          </div>
+        </div>
       </div>
-
-    </div>
+    
   )
 }
 
