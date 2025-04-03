aa44d8d16146b893583f6b639501691655e8395c
JONATHAN STULP ZOZ <jonathan_zoz@estudante.sesisenai.org.br>
2025-04-03 16:36:23 -0300

    modalzim

--- VidaFit/src/pages/EditPerfil.jsx
+++ VidaFit/src/pages/EditPerfil.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import Navbar from '../Components/Navbar'
 import './EditPerfil.css'
-import { useStates } from "react"
-
+import { useState } from 'react'
+import Modal from '../Components/modalConfirmProficional'
 function EditPerfil() {
+  const [openModal, setOpenModal] = useState(false)
+  
   return (
    <div className='container-editperfil'>
             <Navbar />
@@ -22,7 +23,7 @@ function EditPerfil() {
             <div className="espaco"></div>
 
             <div className="perfil-input">
-             
+          
               <input className='texto-inp-edit' type="text" placeholder='Nome :'/>
               <input className='texto-inp-edit' type="text" placeholder='Data de nascimento :'/>
               <input className='texto-inp-edit' type="text" placeholder='Email :'/>
@@ -38,11 +39,12 @@ function EditPerfil() {
 
         <div className='container-inputs'>
            <div className='div-inputs1'>
-            <input className='texto-inp-inf' type="text" placeholder="Input 1"/>
+            <input className='texto-inp-inf' type="" placeholder="Input 1"/>
             <input className='texto-inp-inf' type="text" placeholder="Input 2"/>
             <input className='texto-inp-inf' type="text" placeholder="Input 3"/>
             <input className='texto-inp-inf' type="text" placeholder="Input 4"/>
-           </div>
+           </div>  
+  
 
           <div className='div-inputs2'>
             <input type="text" placeholder="Input 6"/>
@@ -51,9 +53,13 @@ function EditPerfil() {
           
             <div className='container-buttom'>
             <h2>deseja ser um profisional ?</h2>
-            <button className="butoon-click-1" onClick={() => alert("clicou!!!!!")}>clique aqui</button>
+            
+            <button className="butoon-click-1" onClick={() => setOpenModal(true)}>clique aqui</button>
             </div>
-
+            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+        Conteúdo do modal
+      </Modal>
+      
           </div>
 
         </div>
