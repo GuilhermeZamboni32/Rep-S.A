dfbff9a1113a83c9f7df082c6a0f2ea4bf8fc6d4
Jow-Sky <jonyjonathanzoz@gmail.com>
2025-04-15 15:33:09 -0300

    ajustes no edit user

--- VidaFit/src/pages/EditPerfil.jsx
+++ VidaFit/src/pages/EditPerfil.jsx
@@ -39,17 +39,35 @@ function EditPerfil() {
 
         <div className='container-inputs'>
            <div className='div-inputs1'>
-            <input className='texto-inp-inf' type="" placeholder="Input 1"/>
-            <input className='texto-inp-inf' type="text" placeholder="Input 2"/>
-            <input className='texto-inp-inf' type="text" placeholder="Input 3"/>
-            <input className='texto-inp-inf' type="text" placeholder="Input 4"/>
+           <select className='selectEditPerfil'>
+              <option value="">Horários Disponiveis</option>
+              <option value="1">Manhã</option>
+              <option value="2">Tarde</option>     
+              <option value="3">Noite</option>         
+            </select>
+
+            <select className='selectEditPerfil'>
+              <option value="">comorbidades</option>
+              <option value="1">Sim</option>
+              <option value="2">Não</option>              
+            </select>
+            <input className='texto-inp-inf' type="text" placeholder="Senha Atual:"/>
+            <input className='texto-inp-inf' type="text" placeholder="Nova Senha:"/>
            </div>  
   
 
           <div className='div-inputs2'>
-            <input type="text" placeholder="Input 6"/>
-            <input type="text" placeholder="Input 7"/>
-            <input type="text" placeholder="Input 8"/>
+            <select className='selectEditPerfil'>
+              <option value="">Genero</option>
+              <option value="1">Masculino</option>
+              <option value="2">Femimino</option>     
+              <option value="3">Croissant</option>  
+              <option value="4">Outro</option>  
+              <option value="5">Prefiro não responder</option>           
+            </select>
+            <input className='texto-inp-inf' type="text" placeholder="Endereço:"/>
+            <input className='texto-inp-inf' type="text" placeholder="CPF:"/>
+           
           
             <div className='container-buttom'>
             <h2>deseja ser um profisional ?</h2>
