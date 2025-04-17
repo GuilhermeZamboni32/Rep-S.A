bf237dbf09e427664e31ac2924d5b6c9ead7fad9
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-04-17 17:16:23 -0300

    Card dos Exercícios

--- VidaFit/src/Components/modalConfirmProficional.jsx
+++ VidaFit/src/Components/modalConfirmProficional.jsx
@@ -1,37 +1,28 @@
 import React from 'react'
+import './ModalConfirmProficional.css'
 
 
-const BACKGROUND_STYLE = {
-  position: 'fixed',
-  top: '0',
-  bottom: '0',
-  left: '0',
-  right: '0',
-  backgroundColor: 'rgb(0,0,0, 0.7)',
-  zIndex: '1000'
-}
 
-const MODAL_STYLE = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%,-50%)',
-  padding: '150px',
-  backgroundColor: '#fff',
-  borderRadius: '10px',
-  color: 'black'
-}
 
 export default function Modal({ isOpen, setModalOpen, children }) {
   if (isOpen) {
     return (
-      <div style={BACKGROUND_STYLE}>
-        <div style={MODAL_STYLE}>
-          <div style={{ cursor: 'pointer'}} onClick={setModalOpen}>
-            x
+      <div className='container-modal' >
+        <div className='conteudo-modal' >
+          <div className='conteudo-modal-topo'>
+            <div className='topo-modal-es'></div>
+            <div className='topo-modal-di'>
+              <div className='sair-modal' onClick={setModalOpen}>
+                <img className='X' src="icone-X.png" alt="" />
+              </div>
+            </div>
           </div>
-          <div>{children}</div>
-          <button onClick={setModalOpen}>Fechar</button>
+            <div className='conteudo-modal-meio'>
+              <div className='texto-modal'>{children}</div>
+            </div>
+            <div className='conteudo-modal-baixo'>
+              <button className='button-modal' onClick={setModalOpen}><h2>Fecher</h2></button>
+            </div>
         </div>
       </div>
     )
@@ -39,3 +30,15 @@ export default function Modal({ isOpen, setModalOpen, children }) {
 
   return null
 }
+
+
+
+{/**<div className='container-modal' style={BACKGROUND_STYLE}>
+        <div style={MODAL_STYLE}>
+          <div style={{ cursor: 'pointer'}} onClick={setModalOpen}>
+            x
+          </div>
+          <div>{children}</div>
+          <button onClick={setModalOpen}>Fechar</button>
+        </div>
+      </div> */}
