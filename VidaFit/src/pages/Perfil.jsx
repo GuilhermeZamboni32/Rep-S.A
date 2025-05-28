25732498a3e6ea7d4801d4f8676ba2d40efaa04c
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-05-28 16:52:00 -0300

    nova tela de cadastro e tela de login

--- VidaFit/src/pages/Perfil.jsx
+++ VidaFit/src/pages/Perfil.jsx
@@ -65,7 +65,7 @@ const formatDate = (date) => {
               src={user?.image || 'the-glock.png'}
               alt="Profile"
             />*/}
-            <img className='imagem-teste' src="carafeliz.webp" alt="" />
+            <img className='imagem-teste' src="./Icons/perfil-branco.png" alt="" />
             </div>
 
             <div className="espaco"></div>
