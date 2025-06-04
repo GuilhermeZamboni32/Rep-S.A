3949fef682a7b409b6dd303dd9ff40070622a451
THIAGO QUADRA ARNOLD <thiago_arnold@estudante.sesisenai.org.br>
2025-06-04 17:11:07 -0300

    atuala tela avalia

--- VidaFit/src/pages/Av_notas.jsx
+++ VidaFit/src/pages/Av_notas.jsx
@@ -4,30 +4,52 @@ import { Link } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 import {useState} from 'react'
 
-function Av_notas({estrelas,comentarios}) {
+function Av_notas () {
+  const [filter, setFilter] = useState('')
+  const [pesquisa, setpesquisa] = useState('')
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  };
+
+  const handlepesquisaChange = (e) => {
+    setpesquisa(e.target.value)
+  };
+
   return (
+    
     <div className="container-Ava">
     <Navbar />
-      <div className="div-grupo-Ava">
+      <div className="div-grupo-Ava-3">
         <div className="Av_notas">
-
           <div className="titulo_av_notas">
             <h1>  
             Avaliaçoes
             </h1>
-
           </div>
-        
-      <div className='cards_av'>
-        <div className='nome-dieta-b'>
-          <h1>{estrelas}</h1>
+          <div className='barra-filtro'>
+          <img className='filtro' src="./Icons/Filtro-2.png" alt="Filtro" />
+          <select 
+            value={filter} 
+            onChange={handleFilterChange}
+            className='select-filtro'
+          >
+            <option value="">filtro</option>
+            <option value="1.5_estrela">1 estrela meia </option>
+            <option value="1_estrela">1 estrela inteira </option>
+            <option value="2.5_estrela">2 estrela meia </option> 
+            <option value="2_estrela">2 estrela inteira  </option>    
+            <option value="3.5_estrela">3 estrela meia </option>
+            <option value="3_estrela">3 estrela inteira  </option>   
+            <option value="4.5_estrela">4estrela meia </option>
+            <option value="4_estrela">4 estrela inteira </option> 
+            <option value="5.5_estrela">5 estrela meia </option>    
+            <option value="5 estrela">5 estrela inteira </option>       
+          </select>
         </div>
-      </div>
-
-      <div className='Av_descriçao'>
-        <h2>{comentarios}</h2>
-      </div>
-
+        
+     
+      
        
         </div>
 
