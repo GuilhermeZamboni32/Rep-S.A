a7b64d456b3f3097d25ae2f7d560b9c7252b4723
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-06-10 14:31:46 -0300

    ajustes no css do perfil

--- VidaFit/src/pages/EditPerfil.jsx
+++ VidaFit/src/pages/EditPerfil.jsx
@@ -117,6 +117,7 @@ console.log(user);
     navigate(-1);
   }
   
+
   return (
    <div className='container-editperfil'>
             <Navbar />
@@ -202,46 +203,50 @@ console.log(user);
             </div>
 
         <div className='container-inputs'>
-           <div className='div-inputs1'>
-           <select className='selectEditPerfil'>
-              <option value="">Horários Disponiveis</option>
-              <option value="1">Manhã</option>
-              <option value="2">Tarde</option>     
-              <option value="3">Noite</option>      
-              <option value="4">Variado</option>   
-            </select>
+        <div className='container-de-inputs-12'>
+              <div className='div-inputs1'>
+                  <select className='selectEditPerfil'>
+                      <option value="">Horários Disponiveis</option>
+                      <option value="1">Manhã</option>
+                      <option value="2">Tarde</option>     
+                      <option value="3">Noite</option>      
+                      <option value="4">Variado</option>   
+                    </select>
 
-            <select className='selectEditPerfil'>
-              <option value="">comorbidades</option>
-              <option value="1">Sim</option>
-              <option value="2">Não</option>              
-            </select>
-            <input className='texto-inp-inf' type="text" placeholder="Senha Atual:"/>
-            <input className='texto-inp-inf' type="text" placeholder="Nova Senha:"/>
-           </div>  
+                    <select className='selectEditPerfil'>
+                      <option value="">comorbidades</option>
+                      <option value="1">Sim</option>
+                      <option value="2">Não</option>              
+                    </select>
+
+                    <select className='selectEditPerfil'>
+                      <option value="">Genero</option>
+                      <option value="1">Masculino</option>
+                      <option value="2">Femimino</option>     
+                      <option value="3">Croissant</option>  
+                      <option value="4">Outro</option>  
+                      <option value="5">Prefiro não responder</option>           
+                    </select>
+              </div>  
+
+
+              <div className='div-inputs2'>
+                    <input className='texto-inp-inf' type="text" placeholder="Endereço:"/>
+                    
+                    <input className='texto-inp-inf' type="text" placeholder="Senha Atual:"/>
+                    <input className='texto-inp-inf' type="text" placeholder="Nova Senha:"/>
+                  
+          
+              </div>
+        </div>
+                    <div className='container-buttom'>
+                    <h2>deseja ser um profisional ?</h2>
+                    
+                    <button className="butoon-click-1" onClick={() => setOpenModal(true)}>clique aqui</button>
+                    </div>
+                    <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}></Modal>
   
 
-          <div className='div-inputs2'>
-            <select className='selectEditPerfil'>
-              <option value="">Genero</option>
-              <option value="1">Masculino</option>
-              <option value="2">Femimino</option>     
-              <option value="3">Croissant</option>  
-              <option value="4">Outro</option>  
-              <option value="5">Prefiro não responder</option>           
-            </select>
-            <input className='texto-inp-inf' type="text" placeholder="Endereço:"/>
-            <input className='texto-inp-inf' type="text" placeholder="CPF:"/>
-           
-            <div className='container-buttom'>
-            <h2>deseja ser um profisional ?</h2>
-            
-            <button className="butoon-click-1" onClick={() => setOpenModal(true)}>clique aqui</button>
-            </div>
-            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
-      </Modal>
-      
-          </div>
 
         </div>
 
